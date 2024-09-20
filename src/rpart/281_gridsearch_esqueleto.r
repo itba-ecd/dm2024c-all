da918e82cbe53d29bff42d94533777c946d31783
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-09-20 15:15:46 +0000

    auto commit 14       20240920 151535

--- src/rpart/281_gridsearch_esqueleto.r
+++ src/rpart/281_gridsearch_esqueleto.r
@@ -145,7 +145,7 @@ tb_grid_search_detalle <- data.table(
 for (vmax_depth in c(3, 6, 12, 18)) {
   for (vmin_split in c(1000, 800, 600, 400, 200)) {
     for (cp_valor in c(-1)) {
-      for (minbucket_valor in c(200)) {
+      for (minbucket_valor in c(300)) {
         
       }
       
