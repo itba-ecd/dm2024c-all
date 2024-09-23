e532fecb29405e760bc615626a4ad1c6c611a77f
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-09-23 14:43:49 +0000

    auto commit 23       20240923 144338

--- src/lightgbm/421_lightgbm_final.r
+++ src/lightgbm/421_lightgbm_final.r
@@ -164,7 +164,7 @@ setorder(tb_entrega, -prob)
 # genero archivos con los  "envios" mejores
 # suba TODOS los archivos a Kaggle
 
-cortes <- seq(1000, 2000, by = 100)
+cortes <- seq(1500, 3000, by = 100)
 for (envios in cortes) {
   tb_entrega[, Predicted := 0L]
   tb_entrega[1:envios, Predicted := 1L]
